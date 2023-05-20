286b5f43c9f0bebd86bd7639c72f6be77c30e70d
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-20 21:49:34 +0200

    update setup files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "breakpoint": "850px"
+    },
+    extend: {
+      colors: {
+        "menu-glass": "#00000000" 
+      }
+    },
   },
   plugins: [],
 }
-
