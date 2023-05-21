98bbc7a899f362a1df9f2be8e512ead8588d42c6
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-21 22:15:19 +0200

    update gitignore and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     screens: {
-      "breakpoint": "850px"
+      "breakpoint": "850px",
+      "tbp": "750px"
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "edge-hex": "url('src/assets/backgrounds/bg-edge-hex.jpg')",
+        "edge-rounds": "url('src/assets/backgrounds/bg-edge-rounds.jpg')",
+        "edge-sq-blocks": "url('src/assets/backgrounds/bg-edge-sq-blocks.jpg')",
+        "rect": "url('src/assets/backgrounds/bg-rect.jpg')"
+      }
+    },
   },
   plugins: [],
 }
