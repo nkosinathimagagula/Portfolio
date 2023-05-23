912e3e4516bf5d16223a882ff73ec96e1c2cef49
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-23 21:16:45 +0200

    add file to export icons

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import close  from "./icons/close.png";
+import menu from "./icons/menu.png";
+import logo from "./logo.png";
+import backend from "./icons/backend.png";
+import aws from "./icons/aws.png";
+import machine_learning from "./icons/ml.png"
+
+
+export { logo, close, menu, backend, aws, machine_learning }
