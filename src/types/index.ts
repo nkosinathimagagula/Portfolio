28ce75f2b3f4f1651f28f1dda73b5b459ed1ca4b
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-23 21:19:57 +0200

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type interest = {
+    name: string;
+    icon: string;
+}
+
+export type experience = {
+    title: string;
+    company_name: string;
+    icon: string;
+    date: string;
+    points: string[];
+}
