582e55b5ad4b75f55eef0ae9e51d026678a53416
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-24 23:20:45 +0200

    declare reshake module

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module "reshake" {
+    export const Shake: React.FunctionComponent<
+        {
+            children?: React.ReactNode;
+            h?: number;
+            v?: number;
+            r?: number;
+            dur?: number;
+        }
+    >;
+}
