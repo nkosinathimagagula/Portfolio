964fe5ccffb06d141d3c16e6db4134dd6b505222
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-25 00:10:05 +0200

    add icons and images. Update assets exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,24 @@ import logo from "./logo.png";
 import backend from "./icons/backend.png";
 import aws from "./icons/aws.png";
 import machine_learning from "./icons/ml.png"
+import cinema_booking from "./projects/cinemaBooking.png";
+import github from "./icons/github.png";
+import Java from "./icons/java.png";
+import Python from "./icons/python.png";
+import HTML from "./icons/html.png";
+import CSS from "./icons/css.png";
+import JavaScript from "./icons/javascript.png";
+import TypeScript from "./icons/typescript.png";
+import ReactJS from "./icons/reactjs.png";
+import Flask from "./icons/flask.png";
+import FastAPI from "./icons/fastapi.svg";
+import Tailwindcss from "./icons/tailwindcss.png";
+import Git from "./icons/git.png"
+import SQL from "./icons/sql.png"
 
 
-export { logo, close, menu, backend, aws, machine_learning }
+export { 
+    logo, close, menu, backend, aws, machine_learning, cinema_booking, github, 
+    Java, Python, HTML, CSS, JavaScript, TypeScript, ReactJS, Flask, FastAPI, 
+    Tailwindcss, Git, SQL 
+}
