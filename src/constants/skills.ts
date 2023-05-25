8adfedb7f11a9d03b653952e774532b7c7c14ef7
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-25 10:11:27 +0200

    add skills

---
+++ src/constants/skills.ts
@@ -0,0 +1,56 @@
+import { skill } from "../types";
+import { 
+    Java, Python, HTML, CSS, JavaScript, TypeScript, 
+    ReactJS, Flask, FastAPI, Tailwindcss, Git, SQL 
+} from "../assets";
+
+export const skills: skill[] = [
+    {
+        name: "Python",
+        icon: Python
+    },
+    {
+        name: "Java",
+        icon: Java
+    },
+    {
+        name: "JavaScript",
+        icon: JavaScript
+    },
+    {
+        name: "TypeScript",
+        icon: TypeScript
+    },
+    {
+        name: "HTML",
+        icon: HTML
+    },
+    {
+        name: "CSS",
+        icon: CSS
+    },
+    {
+        name: "ReactJS",
+        icon: ReactJS
+    },
+    {
+        name: "Flask",
+        icon: Flask
+    },
+    {
+        name: "FastAPI",
+        icon: FastAPI
+    },
+    {
+        name: "Tailwind CSS",
+        icon: Tailwindcss
+    },
+    {
+        name: "Git",
+        icon: Git
+    },
+    {
+        name: "SQL",
+        icon: SQL
+    }
+]
