2faff890fd8b682209ebb111e55713e6cef52f83
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-25 10:12:13 +0200

    add work experience

---
+++ src/constants/experiences.ts
@@ -0,0 +1,16 @@
+import { aws } from "../assets";
+import { experience } from "../types";
+
+export const experiences: experience[] = [
+    {
+        title: "Software Development Engineer Intern",
+        company_name: "Amazon Web Services",
+        icon: aws,
+        date: "November 2022 - February 2023",
+        points: [
+            "I learned a new stack (React/TypeScript) that I used to develop a web application.",
+            "I gained experience in using React with Typescript to create components that can be reusable and managing state across the application.",
+            "I gained experience with working with APIs as I integrated APIs into the application."
+        ]
+    }
+]
